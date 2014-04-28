73d413e1148f84fc9425bde357a89f99a269aa8c
Dmitry Davydov <haudvd@gmail.com>
2014-04-29 01:10:42 +0400

    normal request worker

--- src/request_worker.erl
+++ src/request_worker.erl
@@ -11,26 +11,57 @@ start_link() ->
 request(Link, Pid) ->
   gen_server:cast(?MODULE, {request, Link, Pid}).
 
+%% FIXME maybe free_link, or link_worker
+process_next_deferred_link() ->
+  gen_server:cast(?MODULE, free_link).
+
 init([]) ->
-  {ok, []}.
+  %% TODO add application enviroment
+  Limit = 100,
+  {ok, {Limit, [], []}}.
 
 handle_call(_Message, _From, State) ->
-  { reply, invalid_command, State }.
+  {reply, invalid_command, State}.
 
-handle_cast({ request, Link, Pid }, State) ->
-  {ibrowse_req_id, ReqID} = ibrowse:send_req(Link, [], get, [], [{stream_to, self()}]),
-  { noreply, [ReqID | State] };
+%% TODO maybe records? как то зафиксировать, чтобы не зависело от порядка
+handle_cast(free_link, {Limit, RequestsState, [{Link, Pid} | Tail]}) ->
+  gen_server:cast(?MODULE, {request, Link, Pid}),
+  {noreply, {Limit, RequestsState, Tail}};
+handle_cast({request, Link, Pid}, {0, RequestsState, DeferredLinks}) ->
+  DeferredLink = {Link, Pid},
+  {noreply, {0, RequestsState, [DeferredLink | DeferredLinks]}};
+handle_cast({request, Link, Pid}, {Limit, RequestsState, DeferredLinks}) ->
+  {ibrowse_req_id, ReqId} = ibrowse:send_req(Link, [], get, [], [{stream_to, self()}]),
+  RequestState = {Pid, Link, ReqId, []},
+  {noreply, {Limit - 1, [RequestState | RequestsState], DeferredLinks};
 handle_cast(_Message, State) ->
-  { noreply, State }.
+  {noreply, State}.
 
-handle_info({ibrowse_async_headers, ReqID, StatusCode, _Headers}, State) ->
-
-  { noreply, State };
-handle_info({ibrowse_async_response, ReqID, Body) ->
-  { noreply, State }.
+handle_info({ibrowse_async_headers, ReqId, StatusCode, _Headers}, {Limit, RequestsState, DefLinks}) ->
+  NewRequestsState = process_response(ReqId, RequestsState, {status_code, StatusCode}),
+  {noreply, {Limit, NewRequestsState, DefLinks}};
+handle_info({ibrowse_async_response, ReqId, Body}, State) ->
+  NewRequestsState = process_response(ReqId, RequestsState, {body, Body}),
+  {noreply, {Limit, NewRequestsState, DefLinks}}.
   
 terminate(_Reason, _State) ->
   ok.
 
 code_change(_OldVersion, State, _Extra) ->
-  { ok, State }.
+  {ok, State}.
+
+process_response(ReqId, {Limit, RequestsState, DefLinks}, {Key, Value}) ->
+  % FIXME плохая идея - привязываться к порядку
+  {value, {Pid, Link, ReqId, ResponseChunks}, StateExceptCurrent} = lists:keytake(ReqId, 3, RequestsState),
+  NewState = case ResponseChunks of
+    [{status_code, StatusCode}] ->
+      Pid ! {Link, StatusCode, Value};
+      process_next_deferred_link(),
+      {Limit + 1, StateExceptCurrent, DeferredLinks};
+    [{body, Body}] ->
+      Pid ! {Link, Value, Body};
+      process_next_deferred_link(),
+      {Limit + 1, StateExceptCurrent, DeferredLinks};
+    [] ->
+      {Limit, [{Pid, Link, ReqId, [{Key, Value}]} | StateExceptCurrent], DeferredLinks}
+  end.
