5eb1c4b3e8c84e930db84254c3429471a261cc78
dotu <a.safronov87u@example.com>
2014-04-29 15:52:36 +0400

    refactor

--- src/linkchecker_worker.erl
+++ src/linkchecker_worker.erl
@@ -5,7 +5,6 @@
 -export([init/1, handle_call/3, handle_cast/2,
          handle_info/2, terminate/2, code_change/3]).
 
--import(httpc,[request/4]).
 
 start_link(Domain, Limit, Pid) ->
   gen_server:start_link({ local, ?MODULE}, ?MODULE, [Domain, Limit, Pid], []).
@@ -14,18 +13,22 @@ start_link(Domain, Limit, Pid) ->
 
 init([Domain, Limit, Pid]) ->
   gen_server:cast(?MODULE, { check, Domain, Limit, Pid }),
-  {ok, []}.
+  {ok, {Domain, [], sets:new(), Pid, Limit, 1}}.
 
 handle_call(_Message, _From, State) ->
   { reply, invalid_command, State }.
 
 handle_cast({ check, Domain, Limit, Pid }, State) ->
-  gen_server:cast(request_worker, {request, Domain, self()}),
-  check_all(Domain, Limit, Pid),
+  cast_request_worker(Domain),
   { noreply, State };
 handle_cast(_Message, State) ->
   { noreply, State }.
 
+handle_info({response, Link, StatusCode, Body}, {Domain, ProcessedLinks, Visited, ClientPid, Limit, RequestCount}) ->
+  Links = get_links(Body, Domain),
+  NewVisited = proccess_links(Links, Visited),
+
+  { noreply, {Domain, [{StatusCode, Link} | ProcessedLinks], NewVisited, ClientPid, Limit, RequestCount - 1} };
 handle_info(_Message, State) ->
   { noreply, State }.
 
@@ -36,44 +39,55 @@ code_change(_OldVersion, State, _Extra) ->
   { ok, State }.
 
 % LinkChecker Impl
+cast_request_worker(Link) ->
+  gen_server:cast(request_worker, {request, Link, self()}).
 
-check_all(URL, LinksCount, Pid) ->
-  case http_uri:parse(URL) of
-    {ok, {http,_, Domain, _, _, _}} ->
-      Set = sets:new(),
-%      spawn_link(link_checker, collect_links, [[URL], Set, Domain, LinksCount, self()]);
-      collect_links([URL], Set, Domain, LinksCount, Pid);
-    {ok, {https, _, _, _, _, _}} -> {error, not_working_with_https};
-    Error -> Error
-  end.
-
-collect_links(Links, _, _, Limit, Pid) when Links == [] orelse Limit == 0 ->
-  Pid ! {ok, proccess_end};
-collect_links([Current | Rest], VisitedExceptCurrent, Domain, Limit, Pid) ->
-  timer:sleep(200),
-  case sets:is_element(Current, VisitedExceptCurrent) of
+proccess_links([], Visited) ->
+  Visited;
+proccess_links([Current | RestLinks], Visited) ->
+  NewVisited = case sets:is_element(Current, Visited) of
     false ->
-      Visited = sets:add_element(Current, VisitedExceptCurrent),
-      case request(Current) of
-        {200, Body} ->
-          Pid ! {200, Current},
-          ToCheck = get_links(Body, Domain) ++ Rest,
-          collect_links(ToCheck, Visited, Domain, Limit - 1, Pid);
-        {Status, _Body} ->
-          Pid ! {Status, Current}, 
-          collect_links(Rest, Visited, Domain, Limit - 1, Pid)
-      end;
+      cast_request_worker(Current),
+      sets:add_element(Current, Visited);
     true ->
-      collect_links(Rest, VisitedExceptCurrent, Domain, Limit, Pid)
-  end.
+      Visited
+  end,
+  proccess_links(RestLinks, NewVisited).
 
-request(URL) ->
-  case httpc:request(get, {URL, []}, [{timeout, timer:seconds(10)}], []) of
-    {ok, {{_, StatusCode, _}, _, Body}} ->
-      {StatusCode, Body};
-    _ -> 
-      {timeout, URL}
-  end.
+
+
+% check_all(URL, LinksCount, Pid) ->
+%   case http_uri:parse(URL) of
+%     {ok, {http,_, Domain, _, _, _}} ->
+%       Set = sets:new(),
+% %      spawn_link(link_checker, collect_links, [[URL], Set, Domain, LinksCount, self()]);
+%       collect_links([URL], Set, Domain, LinksCount, Pid);
+%     {ok, {https, _, _, _, _, _}} -> {error, not_working_with_https};
+%     Error -> Error
+%   end.
+
+% collect_links(Links, _, _, Limit, Pid) when Links == [] orelse Limit == 0 ->
+%   Pid ! {ok, proccess_end};
+% collect_links([Current | Rest], VisitedExceptCurrent, Domain, Limit, Pid) ->
+%   timer:sleep(200),
+%   case sets:is_element(Current, VisitedExceptCurrent) of
+%     false ->
+%       Visited = sets:add_element(Current, VisitedExceptCurrent),
+%       case request(Current) of
+%         {200, Body} ->
+%           Pid ! {200, Current},
+%           ToCheck = get_links(Body, Domain) ++ Rest,
+%           collect_links(ToCheck, Visited, Domain, Limit - 1, Pid);
+%         {Status, _Body} ->
+%           Pid ! {Status, Current}, 
+%           collect_links(Rest, Visited, Domain, Limit - 1, Pid)
+%       end;
+%     true ->
+%       collect_links(Rest, VisitedExceptCurrent, Domain, Limit, Pid)
+%   end.
+
+
+
 
 get_links(Body, Domain) ->
   ExtractedLinks = extract_links(Body, Domain),
